2148b01a6e7262183fa8b1dfa5e8efb1677888c1
EPodesta <epodesta158@gmail.com>
2015-09-12 10:30:41 -0300

    Test

--- exerciseProcedural.py
+++ exerciseProcedural.py
@@ -1,7 +1,11 @@
-#import urllib, json
-#import sys
-#url = sys.argv[1]
-#response = urllib.urlopen(url)
-#data = json.loads(response.read())
-#print data
+import requests
+import json
+import urllib
 
+f = raw_input('Enter currency to convert from(Currency Code): ')
+t = raw_input('Enter the currency to convert to(Currency Code): ')
+
+value = float(raw_input('Enter value to convert: '))
+url = urllib.urlopen(('https://www.exchangerate-api.com/%s/%s/%F?k=API_KEY') % (f, t, value))
+res = url.read()
+print res
